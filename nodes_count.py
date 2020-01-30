1dce057c811fbb292d7ab077d1e47345a0ee6d99
Rednewspaper <59767850+Rednewspaper@users.noreply.github.com>
2020-01-30 12:03:01 +0100

    Update nodes_count.py

--- nodes_count.py
+++ nodes_count.py
@@ -1,8 +1,10 @@
 import os
 
-os.system('nmap -p 61591 10.0.0.0/24 -oN result.txt | grep open')
+def countNodes():
+  
+    os.system('nmap -p 61591 10.0.0.0/24 -oN result.txt | grep open')
+    data = open('result.txt').read()
+    count_nodes = data.count('open')
+    data.close()
 
-data = open('result.txt').read()
-count_nodes = data.count('open')
-
-os.remove('result.txt')
+    os.remove('result.txt')
